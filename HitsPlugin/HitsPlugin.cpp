7042ea17bea136891f686f4d924ce98f773c4d02
Caleb <psipyro@gmail.com>
2019-08-23 18:47:23 -0700

    added speed overlay in metric and american

--- HitsPlugin/HitsPlugin.cpp
+++ HitsPlugin/HitsPlugin.cpp
@@ -1,4 +1,5 @@
 #include "HitsPlugin.h"
+#include "HelperFunctions.h"
 BAKKESMOD_PLUGIN(HitsPlugin, "HitsPlugin ", "0.1", PLUGINTYPE_FREEPLAY);
 
 /*
@@ -16,140 +17,255 @@ HitsPlugin::~HitsPlugin()
 {
 }
 
+void HitsPlugin::listCvars()
+{
+	cvarManager->log("hitsplugin_hits_show " + hp::BoolToString(cvarManager->getCvar("hitsplugin_hits_show").getBoolValue()));
+	cvarManager->log("hitsplugin_hits_show_always " + hp::BoolToString(cvarManager->getCvar("hitsplugin_hits_show_always").getBoolValue()));
+	cvarManager->log("hitsplugin_hits_amount " + std::to_string(cvarManager->getCvar("hitsplugin_hits_show_always").getIntValue()));
+}
+
 void HitsPlugin::onLoad()
 {
 	ServerWrapper server_wrapper = gameWrapper->GetGameEventAsServer();
 
 	hitsShow = make_shared<bool>(true);
-	cvarManager->registerCvar("hitsplugin_hits_show", "1", "Show Hits", true, true, 0, true, 1).bindTo(hitsShow);
-	cvarManager->getCvar("hitsplugin_hits_show").addOnValueChanged(std::bind(&HitsPlugin::OnHitboxOnValueChanged, this, std::placeholders::_1, std::placeholders::_2));
-	//ss
+	hitsShowAlways = make_shared<bool>(true);
+	hitsShowAmount = make_shared<int>(true);
+	cvarManager->registerCvar("hitsplugin_hits_show", "1", "Attempt to enable display of hits right now.", false, true, 0, true, 1).bindTo(hitsShow);
+	cvarManager->registerCvar("hitsplugin_hits_show_always", "1", "Automatically enable hits whenever possible (freeplay and training)", false, true, 0, true, 1).bindTo(hitsShowAlways);
+	cvarManager->registerCvar("hitsplugin_hits_amount", "5", "", false, true, 1, true, HITS_ARRAY_SIZE -1).bindTo(hitsShowAmount);
+	cvarManager->registerNotifier("hits_list_hits", [&gw = this->gameWrapper, this](vector<string> commands) {
+		if (!gw->IsInFreeplay())
+			return;
+		// list all debug info about vectors
+		int drawIndex = hitsIndex;
+		bool stillListing = true;
+		cvarManager->log("HitsPlugin hits currently showing:");
+		while (stillListing)
+		{
+			drawIndex -= 1;
+			if (drawIndex < 0)
+				drawIndex = *hitsShowAmount;
+			if (drawIndex == hitsIndex)
+				stillListing = false;
 
+			cvarManager->log(std::to_string(hits[drawIndex].hitPowerAmbiguous))
+			;
+		}
+	}, "Shows the car speedometer, ball speedometer, and boost usage meter.", PERMISSION_FREEPLAY | PERMISSION_PAUSEMENU_CLOSED);
+	
+	cvarManager->registerNotifier("hits", [&gw = this->gameWrapper, this](vector<string> commands) {
+		if (!gameWrapper->IsInFreeplay() && !gameWrapper->IsInCustomTraining())
+			return;
+		if (commands.size() >=2)
+		{
+			if (commands.at(1)._Equal("off"))
+			{
+				cvarManager->getCvar("hitsplugin_hits_show").setValue(false);
+				cvarManager->log("Hits OFF");
+			}
+			// hits show
+			if (commands.at(1)._Equal("on"))
+			{
+				if (commands.size() == 2)
+				{
+					cvarManager->getCvar("hitsplugin_hits_show").setValue(true);
+					cvarManager->log("Hits ON");
+				}
+				else
+				{
+					// hits show always
+					if (commands.at(2)._Equal("always"))
+					{
+						cvarManager->getCvar("hitsplugin_hits_show_always").setValue(true);
+						cvarManager->log("Hits will show automatically.");
+					}
+					// hits show never
+					if (commands.at(2)._Equal("never"))
+					{
+						cvarManager->getCvar("hitsplugin_hits_show_always").setValue(false);
+						cvarManager->log("Hits will not be shown automatically.");
+					}
+				}
+			}
+			if (commands.at(1)._Equal("show"))
+			{
+				if(commands.size() == 2)
+				{
+					cvarManager->log("Showing " + std::to_string(cvarManager->getCvar("hitsplugin_hits_amount").getIntValue()) + " hits");
+				}
+				if (commands.size() == 3)
+				{
+					int newHitsAmount = 1;
+					if (hp::str2int(newHitsAmount, commands.at(2).c_str()) == hp::SUCCESS)
+					{
+						setNumberOfHits(newHitsAmount);
+					} else
+					{
+						cvarManager->log("Number of hits must be a number.");
+					}
+				}
+			}
+			// hits hide
 
-	gameWrapper->HookEvent("Function TAGame.GameEvent_Tutorial_TA.OnInit", bind(&HitsPlugin::OnFreeplayLoad, this, std::placeholders::_1));
-	gameWrapper->HookEvent("Function TAGame.GameEvent_Tutorial_TA.Destroyed", bind(&HitsPlugin::OnFreeplayDestroy, this, std::placeholders::_1));
-	gameWrapper->HookEvent("Function TAGame.GameEvent_TrainingEditor_TA.StartPlayTest", bind(&HitsPlugin::OnFreeplayLoad, this, std::placeholders::_1));
-	gameWrapper->HookEvent("Function TAGame.GameEvent_TrainingEditor_TA.Destroyed", bind(&HitsPlugin::OnFreeplayDestroy, this, std::placeholders::_1));
-	gameWrapper->HookEventWithCaller<CarWrapper>("Function TAGame.Car_TA.OnHitBall",
-		std::bind(&HitsPlugin::onHitBallWithCaller,
-			this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+		} else
+		{
+			listCvars();
+		}
+		
+	}, "Shows the car speedometer, ball speedometer, and boost usage meter.", PERMISSION_FREEPLAY | PERMISSION_PAUSEMENU_CLOSED);
 
-	// TutorialWrapper.GetSecondsElapsed() - best way of calculating time passing because it’s tied to physics tick
+	gameWrapper->HookEventWithCaller<CarWrapper>("Function TAGame.Car_TA.OnHitBall",std::bind(&HitsPlugin::onHitBallWithCaller,this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+
 	//Cvars
 	// hits - describes available functions
-
 	// hits show - turns on the plugin
-
 	// hits hide - turns off the plugin
-
 	// hits set 2 - limit the amount of hits to show to 2 (min 1, max 1000)
-
 	// hits dim on - scales the alpha of hits based on how long they've been showing
-
 	// hits color - tells the current color
-
 	// hits color on - changes color based on how far back in the list they are
-
 	// hits color red - changes the color that is used for hits to red
-	cvarManager->executeCommand("hitsplugin_hits_show 1");
+	if (*hitsShowAlways)
+		cvarManager->getCvar("hitsplugin_hits_show").setValue(true);
+
+	*hitsShowAmount = cvarManager->getCvar("hitsplugin_hits_amount").getIntValue();
+
+	SetupAndStartDrawing();
+	listCvars();
+
+	cvarManager->log("HitsPlugin loaded. Will only show the hits in freeplay and custom training! Created by expendis.");
+}
+
+void HitsPlugin::setNumberOfHits(int numberOfHits)
+{
+	int valueToChangeHitsAmountTo = 1;
+	if (numberOfHits >= HITS_ARRAY_SIZE)
+	{
+		cvarManager->log(std::to_string(numberOfHits) + " is too large. Setting to the max, " + std::to_string((HITS_ARRAY_SIZE - 1)));
+		valueToChangeHitsAmountTo = HITS_ARRAY_SIZE - 1;
+	} else
+	{
+		if (numberOfHits <= 0)
+		{
+			cvarManager->log(std::to_string(numberOfHits) + " is too small. Setting to the min, 1");
+		} else
+		{
+			valueToChangeHitsAmountTo = numberOfHits;
+		}
+	}
+	StopDrawing();
+	SetupAndStartDrawing();
+	*hitsShowAmount = valueToChangeHitsAmountTo;
+	//cvarManager->getCvar("hitsplugin_hits_amount").setValue(valueToChangeHitsAmountTo);
+	cvarManager->log("Now showing " + std::to_string(valueToChangeHitsAmountTo) + " hits.");
 }
 
 void HitsPlugin::onUnload()
 {
+	StopDrawing();
 	cvarManager->backupCfg("./bakkesmod/cfg/config.cfg");
 	gameWrapper->UnhookEvent("Function TAGame.Car_TA.OnHitBall");
 }
 
-void HitsPlugin::OnFreeplayLoad(std::string eventName)
+void HitsPlugin::SetupAndStartDrawing()
 {
-	if (*hitsShow) {
-		//clear the hits array
-		for (int i = 0; i < hitsMax; i++)
-		{
-			hits[i] = { Vector{0,0,0} };
-		}
-		hitsIndex = 0;
-		gameWrapper->RegisterDrawable(std::bind(&HitsPlugin::Render, this, std::placeholders::_1));
-		cvarManager->log("HitsPlugin started drawing");
+	//clear the hits array
+	for (int i = 0; i < HITS_ARRAY_SIZE; i++)
+	{
+		hits[i] = { Vector{0,0,0} };
 	}
+	hitsIndex = 0;
+	gameWrapper->RegisterDrawable(std::bind(&HitsPlugin::Render, this, std::placeholders::_1));
+	cvarManager->log("HitsPlugin drawing enabled.");
 }
 
-void HitsPlugin::OnFreeplayDestroy(std::string eventName)
+void HitsPlugin::StopDrawing()
 {
 	gameWrapper->UnregisterDrawables();
 	cvarManager->log("HitsPlugin stopped drawing");
 }
 
-void HitsPlugin::OnHitboxOnValueChanged(std::string oldValue, CVarWrapper cvar)
-{
-	if (cvar.getBoolValue() && gameWrapper->IsInGame()) {
-		OnFreeplayLoad("Load");
-	}
-	else
-	{
-		OnFreeplayDestroy("Destroy");
-	}
-}
-
 void HitsPlugin::Render(CanvasWrapper canvas)
 {
-	if (*hitsShow && gameWrapper->IsInGame())
-	{
-		ServerWrapper game = gameWrapper->GetGameEventAsServer();
+	if (!gameWrapper->IsInFreeplay() && !gameWrapper->IsInCustomTraining())
+		return;
 
-		if (game.IsNull())
+	if (!*hitsShow)
+		return;
+
+	ServerWrapper game = gameWrapper->GetGameEventAsServer();
+
+	if (game.IsNull())
+		return;
+	ArrayWrapper<CarWrapper> cars = game.GetCars();
+
+	if (cars.Count() > 0) {
+		CarWrapper car = cars.Get(0);
+		if (car.IsNull())
 			return;
-		ArrayWrapper<CarWrapper> cars = game.GetCars();
-
-		if (cars.Count() > 0) {
-			CarWrapper car = cars.Get(0);
-			if (car.IsNull())
-				return;
-			Vector carLocation = car.GetLocation();
-			Rotator carRotation = car.GetRotation();
+		Vector carLocation = car.GetLocation();
+		Rotator carRotation = car.GetRotation();
 
 
-			// Setup values for my code
-			canvas.SetColor(255, 0, 255, 200);
 
-			// LOOP THROUGH downward AND DRAW MY HITS
-			int drawIndex = hitsIndex;
-			bool stillDrawing = true;
-			Vector2 hitIn2d;
-			while (stillDrawing)
-			{
+		// LOOP THROUGH downward AND DRAW MY HITS
+		int drawIndex = hitsIndex;
+		bool stillDrawing = true;
+		while (stillDrawing)
+		{
+			if (*hitsShowAmount != 1)
 				drawIndex -= 1;
-				if (drawIndex < 0)
-					drawIndex = hitsMax;
+
+			if (drawIndex < 0)
+				drawIndex = *hitsShowAmount;
+			if (drawIndex == hitsIndex)
+				stillDrawing = false;
+
+			Vector thisHitLocation = hits[drawIndex].location;
+
+			// if this is actually a hit and not null
+			if (thisHitLocation.X != 0 && thisHitLocation.Y != 0 && thisHitLocation.Z != 0)
+			{
+				// set canvas color to purple
+				Color hitColor = hp::getHitColorForValueInRange(hits[drawIndex].hitPowerAmbiguous, 2500.0f);
+				canvas.SetColor(hitColor.r, hitColor.g, hitColor.b, hitColor.a);
+
+				// get 2d projection of the hitvector, rotated and translated to its proper location relative to car
+				Vector vectorToDraw = getRecalledVectorForCurrentCar(carLocation, carRotation, thisHitLocation);
+
+				// Project the location onto the canvas
+				Vector2 canvasPosition = canvas.Project(vectorToDraw);
+
+				// Set the position of canvas
+				canvas.SetPosition(canvasPosition);
+
+				// draw the hit
 				if (drawIndex == hitsIndex)
-					stillDrawing = false;
-
-				// if this is actually a hit and not null
-				if (hits[drawIndex].X != 0 && hits[drawIndex].Y != 0 && hits[drawIndex].Z != 0)
 				{
-					// set canvas color to purple
-					canvas.SetColor(255, 0, 255, 200);
+					// This is the most recent hit
+					canvas.FillBox(hitRenderSizeMax);
+					canvas.SetColor(255, 255, 255, 255);
 
-					// get 2d projection of the hitvector, rotated and translated to its proper location relative to car
-					hitIn2d = canvas.Project(getRecalledVectorForCurrentCar(carLocation, carRotation, hits[drawIndex]));
-					canvas.SetPosition(hitIn2d);
+					canvas.SetPosition(canvasPosition);
+					canvas.DrawBox(hitRenderSizeMax);
 
-					if (drawIndex == hitsIndex)
-					{
-						//set canvas color to red
-						canvas.SetColor(255, 0, 0, 200);
-					}
-					// draw the hit
-					canvas.FillBox(Vector2{ 15, 15 });
+					// draw the text
+					canvas.SetColor(hitColor.r, hitColor.g, hitColor.b, hitColor.a);
+
+					Vector2 textPosition = canvas.Project(
+						sp::rotateVectorWithQuat(textOffsetFromCar, sp::rotToQuat(carRotation))
+					+ carLocation);
+					canvas.SetPosition(
+						Vector2{ textPosition.X// + textRenderOffset.X
+						,textPosition.Y// + textRenderOffset.Y
+						});
+					canvas.DrawString(hp::speedAsString(hits[drawIndex].hitPowerAmbiguous, false), textScale, textScale);
+					
+				} else
+				{
+					canvas.FillBox(hitRenderSizeMin);
 				}
-			}
-			// Draw a line from the car to the ball
-			// do this to simulate roughly what information get stored into hits but see it live
-			BallWrapper ball = game.GetGameBalls().Get(0);
-			if (!ball.IsNull()) {
-				//Vector vectorFromCarToBall = getVectorToStore(carLocation, carRotation, ball.GetLocation() - carLocation);
-
-				//canvas.DrawLine(canvas.Project(carLocation), canvas.Project(MyRotate(vectorFromCarToBall, dRoll, -dYaw, dPitch) + carLocation));
 			}
 		}
 	}
@@ -157,18 +273,22 @@ void HitsPlugin::Render(CanvasWrapper canvas)
 
 Vector HitsPlugin::getVectorToStore(Vector carLocation, Rotator carRotation, Vector hitLocation)
 {
-	return myGoodRotate(hitLocation - carLocation, carRotation);
+	Vector rotatedVector = hitLocation - carLocation;
+	rotatedVector = sp::rotateVectorWithQuat(rotatedVector, sp::rotToQuat({ 0,-carRotation.Yaw,0 }));
+	rotatedVector = sp::rotateVectorWithQuat(rotatedVector, sp::rotToQuat({ -carRotation.Pitch,0,0 }));
+	rotatedVector = sp::rotateVectorWithQuat(rotatedVector, sp::rotToQuat({ 0,0,-carRotation.Roll }));
+	return rotatedVector;
 }
 
 Vector HitsPlugin::getRecalledVectorForCurrentCar(Vector carLocation, Rotator carRotation, Vector recalledVector)
 {
-	Vector result;
-	double dPitch = (double)carRotation.Pitch / 32764.0 * 3.14159;
-	double dYaw = (double)carRotation.Yaw / 32764.0 * 3.14159;
-	double dRoll = (double)carRotation.Roll / 32764.0 * 3.14159;
-	result = MyRotate(recalledVector, dRoll, -dYaw, dPitch) + carLocation;
-	return result;
+	Vector result = recalledVector;
+	result = sp::rotateVectorWithQuat(result, sp::rotToQuat({ 0,0,carRotation.Roll }));
+	result = sp::rotateVectorWithQuat(result, sp::rotToQuat({ carRotation.Pitch,0,0 }));
+	result = sp::rotateVectorWithQuat(result, sp::rotToQuat({ 0,carRotation.Yaw,0 }));
+	return result + carLocation;
 }
+
 void HitsPlugin::onHitBallWithCaller(CarWrapper caller, void* params, std::string eventName)
 {
 	if (!gameWrapper->IsInFreeplay() && !gameWrapper->IsInCustomTraining())
@@ -178,7 +298,7 @@ void HitsPlugin::onHitBallWithCaller(CarWrapper caller, void* params, std::strin
 	 * always add one before new hit is recorded
 	 */
 	hitsIndex++;
-	if (hitsIndex > hitsMax)
+	if (hitsIndex > *hitsShowAmount)
 	{
 		hitsIndex = 0;
 	}
@@ -194,21 +314,27 @@ void HitsPlugin::onHitBallWithCaller(CarWrapper caller, void* params, std::strin
 		Rotator carRotation = car.GetRotation();
 		Vector hitLocation = hbp.Location;
 
-		//What I'm testing
-		//hits[hitsIndex] = getVectorToStore(carLocation, carRotation, hitLocation);
 
-		//What Works (while the car is flat on the ground)
-		double dPitch = (double)carRotation.Pitch / 32764.0 * 3.14159;
-		double dYaw = (double)carRotation.Yaw / 32764.0 * 3.14159;
-		double dRoll = (double)carRotation.Roll / 32764.0 * 3.14159;
-		hits[hitsIndex] = MyRotate(hitLocation - carLocation, -dRoll, dYaw, -dPitch);
+		float forceToInputForThisHit = 0.0f;
+		//BallWrapper ball = gameWrapper->GetGameEventAsServer().GetGameBalls().Get(0);
+		//if (!ball.IsNull()) {
+		//	forceToInputForThisHit = (ball.GetVelocity() - hbp.NormalVelocity).magnitude();
+		//}
 
+		forceToInputForThisHit = (hbp.Velocity - hbp.OtherVelocity).magnitude();
 
+		hits[hitsIndex] = {
+			getVectorToStore(carLocation, carRotation, hitLocation),
+			forceToInputForThisHit };
+
+		//print the coloro utput to console
+		Color hitColor = hp::getHitColorForValueInRange(forceToInputForThisHit, 4200.0f);//4128 is the max I've seen
 
 	}
-
 }
 
+/* UNUSED - but I might want to use later */
+/*
 Vector HitsPlugin::myGoodRotate(Vector vectorToRotate, Rotator rotation)
 {
 	double dPitch = (double)rotation.Pitch / 16340.0 * 3.14159;
@@ -270,7 +396,7 @@ Vector HitsPlugin::MyRotate(Vector aVec, double roll, double yaw, double pitch)
 	aVec.Y = tmp;
 	return aVec;
 }
-
+*/
 
 
 
--- HitsPlugin/HitsPlugin.h
+++ HitsPlugin/HitsPlugin.h
@@ -1,6 +1,10 @@
 #pragma once
 #pragma comment( lib, "bakkesmod.lib" )
 #include "bakkesmod\plugin\bakkesmodplugin.h"
+#include <ios>
+#include <iomanip>
+#include <sstream>
+constexpr auto HITS_ARRAY_SIZE = 20;
 /*
  * Params for ball collision
  */
@@ -18,6 +22,106 @@ struct HitBallParams
 	int NumCollisions;
 	int NumContacts;
 };
+
+struct Color { int r, g, b, a; };
+
+
+
+
+namespace hp
+{
+	inline string ColorToString(Color color)
+	{
+		return std::to_string(color.r) + " " +
+			std::to_string(color.g) + " " +
+			std::to_string(color.b) + " " +
+			std::to_string(color.a);
+	}
+	inline const string BoolToString(bool b)
+	{
+		return b ? "true" : "false";
+	}
+	inline Color getHitColorForValueInRange(float value, float valueMax)
+	{
+		// I have tweaked these values for what I feel is a nice color gradient
+		float redLow = 60.0f;
+		float redHigh = 255.0f;
+		float greenLow = 30.0f;
+		float greenHigh = 255.0f;
+		Color result = { 0,0,0,200 };
+		float scale = value / valueMax;
+		if (value < valueMax / 2.0f)
+		{
+			result.r = static_cast<int>(redLow + (redHigh - redLow) * scale);
+			result.g = greenHigh;
+		}
+		else
+		{
+			result.r = redHigh;
+			result.g = static_cast<int>(greenHigh - (greenHigh - greenLow) * scale);
+		}
+		return result;
+	}
+
+	enum STR2INT_ERROR { SUCCESS, OVERFLOWS, UNDERFLOWS, INCONVERTIBLE };
+
+	STR2INT_ERROR str2int(int& i, char const* s, int base = 0)
+	{
+		char* end;
+		long  l;
+		errno = 0;
+		l = strtol(s, &end, base);
+		if ((errno == ERANGE && l == LONG_MAX) || l > INT_MAX) {
+			return OVERFLOWS;
+		}
+		if ((errno == ERANGE && l == LONG_MIN) || l < INT_MIN) {
+			return UNDERFLOWS;
+		}
+		if (*s == '\0' || *end != '\0') {
+			return INCONVERTIBLE;
+		}
+		i = l;
+		return SUCCESS;
+	}
+
+	float UupsToMph(float uups)
+	{
+		return uups * 60.0f * 60.0f / 160934.4f;
+
+	}
+
+	float UupsToKph(float uups)
+	{
+
+		return uups * 60.0f * 60.0f / 100000.0f;
+	}
+
+	std::string speedAsString(float speed, bool metric)
+	{
+		std::stringstream stream;
+		if (metric)
+		{
+			stream << std::fixed << std::setprecision(0) << UupsToKph(speed);
+			std::string s = stream.str();
+
+			return s + " km/h";
+		}
+		else
+		{
+			stream << std::fixed << std::setprecision(0) << UupsToMph(speed);
+			std::string s = stream.str();
+
+			return s + " mph";
+		}
+	}
+
+
+}
+struct Hit
+{
+	Vector location;
+	float hitPowerAmbiguous;
+};
 /*
  * The HitsPlugin class
  */
@@ -27,24 +131,35 @@ private:
 
 	//whether hits are shown in-game
 	std::shared_ptr<bool> hitsShow;
-	// whether to
-	int hitsMax = 10;
+	std::shared_ptr<bool> hitsShowAlways;
+	std::shared_ptr<int> hitsShowAmount;
+	int millisecondsDisappearDelay = 1000;
+	Vector2 hitRenderSizeMin = { 10, 10 };
+	Vector2 hitRenderSizeMax = { 15, 15 };
+	Vector2 textRenderOffset = { 250, 0 };
+
+	const float textScale = 2.0f;
+	Vector textOffsetFromCar = { -100,0,0 };
 	void onHitBallWithCaller(CarWrapper caller, void* params, std::string eventName);
-	Vector myGoodRotate(Vector vectorToRotate, Rotator rotation);
-	Vector hits[20] = { Vector{0,0,0} };
+	//Vector myGoodRotate(Vector vectorToRotate, Rotator rotation);
+	Hit hits[HITS_ARRAY_SIZE] = { Hit{{0,0,0}, 0.0f} };
 	int hitsIndex = 0;
 public:
 	virtual void onLoad();
-	void OnFreeplayLoad(std::string eventName);
-	void OnFreeplayDestroy(std::string eventName);
-	void OnHitboxOnValueChanged(std::string oldValue, CVarWrapper cvar);
+	void setNumberOfHits(int numberOfHits);
+	void SetupAndStartDrawing();
+	void StopDrawing();
+	//void OnFreeplayLoad(std::string eventName);
+	//void OnFreeplayDestroy(std::string eventName);
+	//void OnHitboxOnValueChanged(std::string oldValue, CVarWrapper cvar);
 	void Render(CanvasWrapper canvas);
 	Vector getVectorToStore(Vector carLocation, Rotator carRotation, Vector hitLocation);
 	Vector getRecalledVectorForCurrentCar(Vector carLocation, Rotator carRotation, Vector recalledVector);
-	Vector MyRotate(Vector aVec, double roll, double yaw, double pitch);
+	//Vector MyRotate(Vector aVec, double roll, double yaw, double pitch);
 	virtual void onUnload();
 	HitsPlugin();
 
 	~HitsPlugin();
+	void listCvars();
 };
 
